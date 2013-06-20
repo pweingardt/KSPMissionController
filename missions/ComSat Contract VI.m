10eae2851d7802ac93768409ff238e0453bbc9ed
pweingardt <weingardt.dev@googlemail.com>
2013-06-20 12:21:26 +0200

    updated image license, fixed syntax error in one mission

--- missions/ComSat Contract VI.m
+++ missions/ComSat Contract VI.m
@@ -4,6 +4,7 @@ Mission
     description = We signed a contract to bring a satellite into a nearly perfect synchronous, equatorial orbit around Kerbin. They also specified the position of the satellite above the ground.
     repeatable = true
     reward = 100000
+    randomized = true
 
     requiresMission = Kerbolo I
 
@@ -27,7 +28,7 @@ Mission
         maxInclination = 0.02
 
         minLongitude = RANDOM(-180, 179.5)
-        maxLongitude = ADD(minLongitude(0.5)
+        maxLongitude = ADD(minLongitude, 0.5)
 
         minOrbitalPeriod = TIME(5h 59m 55s)
         maxOrbitalPeriod = TIME(6h 5s)
