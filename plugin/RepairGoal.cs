b64252ff760e83dd7a397dde6e167fb2af109e83
malkuth1974 <malkuth@metrocast.net>
2013-09-09 21:46:11 -0400

    Got rid old method MC used for startup

--- plugin/RepairGoal.cs
+++ plugin/RepairGoal.cs
@@ -1,44 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Timers;
 
 namespace MissionController
 {
     /// <summary>
     /// Mission Goal For Repair Vessel
-    /// Does NOt work Yet for some reason
-    /// Done all the checks.. This should work!!
-    /// I even made it more complicated and copied docking and crash, by adding a flag for isrepaired.  Everything changes over and isrepaired becomes true.. But for some reason
-    /// the Else part of this code is not reading it correct?
+    /// Part is loaded in class repairSation and loaded. The Player can Right Click part and set the RepairGoal To True.. 
+    /// Once True the RepairGoal Class takes over and finishes the mission
     /// </summary>
 
     public class repairStation : PartModule
     {
+        Timer time = new Timer();
+        
+
         [KSPField(isPersistant = true)]
         public static bool repair;
+        
 
-        [KSPEvent(guiActive = true, guiName = "Start Repair", active = true)]
+        [KSPEvent(unfocusedRange = 4f, guiActiveUnfocused = true, guiActive = false, guiName = "Start Repair", active = true)]
         public void EnableRepair()
-        {
-            repair = true;            
-        }
-
-        [KSPEvent(guiActive = true, guiName = "End Repair", active = false)]
-        public void DisableRepair()
-        {
-            repair = false;            
-        }
-
-        [KSPAction("Toggle Repair")]
-        public void ToggleRepairAction(KSPActionParam param)
-        {
-            repair = !repair;
-        }
-        public override void OnUpdate()
-        {
-            Events["EnableRepair"].active = !repair;
-            Events["DisableRepair"].active = repair;
-        }
+        {            
+            repair = true;  
+        }      
+        
     }
         
     public class RepairGoal : MissionGoal
