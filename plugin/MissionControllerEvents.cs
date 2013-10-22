60b95ff8e1041fb4c01bd2186fb75e8a6d153af7
malkuth1974 <malkuth@metrocast.net>
2013-10-21 23:02:52 -0400

    fixed the crew insurance window not showing in campaign

--- plugin/MissionControllerEvents.cs
+++ plugin/MissionControllerEvents.cs
@@ -58,13 +58,13 @@ namespace MissionController
             {
                 recycledCost = res.recyclable(pv.situation.Equals(Vessel.Situations.LANDED) ? 1 : 0);
                 print("*MC* Craft " + recycledName + " recovered for " + recycledCost);
-                showRecycleWindow = true;
                 manager.recycleVessel(pv.vesselRef, recycledCost);
             }
             if (settings.difficulty != 0 && (pv.situation.Equals(Vessel.Situations.LANDED) || pv.situation.Equals(Vessel.Situations.SPLASHED)))
             {
                 recycledCrewCost = res.crewreturn(pv.situation.Equals(Vessel.Situations.LANDED) ? 1 : 0);
                 manager.cleanReward(recycledCrewCost);
+                showRecycleWindow = true;
             }
 
             pVessel = null;           
